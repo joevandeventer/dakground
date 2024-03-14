e6b47d8cff0031b4506c5ecb237dae7579e8e090
Joe VanDeventer <fritter@icloud.com>
2024-03-13 22:54:50 -0400

    Initial commit of Dakground

---
+++ dakboard.py
@@ -0,0 +1,22 @@
+from flask import Flask, send_file
+import random
+
+from os import listdir
+from os.path import isfile, join
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+
+  mypath = "./images/"
+
+  onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+  filename = mypath + random.choice(onlyfiles)
+  return send_file(filename, mimetype="image/png")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from dakboard import app
+
+if __name__ == "__main__":
+    app.run()
